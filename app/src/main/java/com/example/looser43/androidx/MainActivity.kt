b82fcdcb922d4a5edf1bf7e94ce3d57748b67038
sam43 <sayemruet.103043@gmail.com>
2018-10-08 00:48:59 +0600

    Material commit

---
+++ app/src/main/java/com/example/looser43/androidx/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.looser43.androidx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
